326a400299ee30e7b6c05a9ebe33b1b1302c0acf
Valeriard <52808016+valeriard@users.noreply.github.com>
2021-03-31 20:33:04 -0500

    Proyecto con los paquetes el pom

---
+++ ProyectoFinal-master/src/test/java/com/seminario/demo/FinalSeminarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seminario.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSeminarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
