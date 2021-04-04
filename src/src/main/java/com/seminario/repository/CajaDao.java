0661c326948e7bcef3eb4109339450d7a27cdab8
Your Name <you@example.com>
2021-04-04 18:15:44 -0500

    Recuperando commit

--- src/src/main/java/com/seminario/repository/CajaDao.java
+++
@@ -1,9 +0,0 @@
-package com.seminario.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.seminario.model.Caja;
-
-public interface CajaDao extends JpaRepository<Caja, Integer>{
-
-}
