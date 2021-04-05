35f57f2e910bf010ef93568cc5d1033cbded4f69
valeriard <valeriard1993@gmail.com>
2021-04-04 19:01:53 -0500

    Modulo caja

---
+++ src/main/java/com/seminario/repository/CajaDao.java
@@ -0,0 +1,16 @@
+package com.seminario.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.seminario.model.Caja;
+@Repository
+public interface CajaDao extends JpaRepository<Caja, Integer>{
+	@Query
+	(value = "select * from caja where fecha_creacion >= ?1 AND  fecha_creacion < ?2", nativeQuery = true)
+	public List<Caja> findByFechaCreacion(String fechaIni, String fechaFin);
+}
