44b717e238b67a91b4ab953347361452c9e0d1bd
Sebastian <25sebasr@gmail.com>
2021-04-07 17:50:21 -0500

    Revert "Finalziacion de Try y cath en service proveedor"
    This reverts commit 3e2ea9d0d1e9899abd211d58604c2baaadd6744c.

--- src/main/java/com/seminario/controller/ProveedorController.java
+++ src/main/java/com/seminario/controller/ProveedorController.java
@@ -42,8 +42,8 @@ public class ProveedorController {
 	}
 	
 	@DeleteMapping("eliminar")
-	public ResponseEntity<Object> eliminarProveedor(@RequestParam Integer id) {
-		return proveedorService.eliminarProveedor(id);
+	public void eliminarProveedor(@RequestParam Integer id) {
+		proveedorService.eliminarProveedor(id);
 	}
 
 }
