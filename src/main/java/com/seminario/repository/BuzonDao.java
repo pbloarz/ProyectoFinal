8f3a36f14b1c4d9aa31d14fd47c3132967c5023d
pbloarz <pabloanrozu@gmail.com>
2021-04-06 21:32:57 -0500

    Reverset

--- src/main/java/com/seminario/repository/BuzonDao.java
+++ src/main/java/com/seminario/repository/BuzonDao.java
@@ -1,10 +1,20 @@
 package com.seminario.repository;
 
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
 import com.seminario.model.Buzon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Transactional
 public interface BuzonDao extends JpaRepository<Buzon,Integer> {
+    
+    
+    @Query(value = "SELECT * FROM buzon where fecha >= ?1 and fecha <= ?2",nativeQuery = true)
+	public List<Buzon> findByRangeDate(String desde, String hasta);
 }
