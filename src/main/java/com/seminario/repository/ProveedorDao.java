e35375a41547450f99a15ec77956873dc2610af7
Sebastian <25sebasr@gmail.com>
2021-04-07 16:39:28 -0500

    Modificacion en el Query Proveedor

--- src/main/java/com/seminario/repository/ProveedorDao.java
+++ src/main/java/com/seminario/repository/ProveedorDao.java
@@ -11,7 +11,7 @@ import com.seminario.model.Proveedor;
 @Repository
 public interface ProveedorDao extends JpaRepository<Proveedor, Integer>{
 	
-	@Query(value = "select * from proveedor where fecha_creacion >= ?1 AND  fecha_creacion < ?2", nativeQuery = true)
+	@Query(value = "select * from proveedor where fecha_creacion >= ?1 AND  fecha_creacion <= ?2", nativeQuery = true)
 	public List<Proveedor> findByFechaCreacion(String fechaIni, String fechaFin);
 
 }
